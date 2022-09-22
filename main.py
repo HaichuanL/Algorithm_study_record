0cfcfe08f646743c5929d1073a6ce87a8651f511
liuhaichuan <haichuanstu@163.com>
2022-09-22 17:24:40 +0800

    update leetcode_35

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Algo:
+    """"""
     '''Fibonacci'''
     def fibo(self, n):
         if n <= 0:
@@ -41,7 +42,6 @@ class Algo:
                 return mid
         return -1
 
-
     def binary_search_v2(self, nums, target):
         """
             704: https://leetcode.cn/problems/binary-search/
@@ -62,7 +62,36 @@ class Algo:
                 return mid
         return -1
 
-    '''双指针使用：删除数组中指定值
+    # binary_search leetcode_35
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        if target in nums:
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    return mid
+        else:
+            if target > nums[r]:
+                return len(nums)
+            elif target < nums[l]:
+                return 0
+            else:
+                for i in range(len(nums)):
+                    if target < nums[i]:
+                        return i
+                        break
+
+    '''删除数组中指定值：双指针使用
     tip1: 为什么要用覆盖'''
     def removeElement(self, nums, val):
         i_1 = 0
@@ -73,6 +102,38 @@ class Algo:
                 i_2 += 1
             i_1 += 1
         return i_2
+
+    '''平方排序：双指针使用，冒泡排序'''
+    # 双指针
+    def sortedSquares_double(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        left, right, k = 0, n - 1, n - 1
+        ans = [0] * n
+        while left <= right:
+            lm = nums[left] ** 2
+            rm = nums[right] ** 2
+            if lm >= rm:
+                ans[k] = lm
+                left += 1
+            else:
+                ans[k] = rm
+                right -= 1
+            k -= 1
+        return ans
+
+    # 冒泡排序
+    def sortedSquares_bubble(self, nums):
+        for i in range(len(nums)):
+            nums[i] = nums[i] ** 2
+        for j in range(len(nums)-1, 0, -1):
+            for i in range(j):
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
 
 
 if __name__ == '__main__':
@@ -91,9 +152,16 @@ if __name__ == '__main__':
     # print(bi_v1)
     # print(bi_v2)
     '''removeElement: 双指针'''
-    nums = [4, 56, 78, 90, 299, 576, 2348, 7890]
-    print('origin nums:', nums)
-    val = 576
-    rem = algo.removeElement(algo, nums, val)
-    print('result:', nums)
-    print(rem)
+    # nums = [4, 56, 78, 90, 299, 576, 2348, 7890]
+    # print('origin nums:', nums)
+    # val = 576
+    # rem = algo.removeElement(algo, nums, val)
+    # print('result:', nums)
+    # print(rem)
+    '''sortedSquares：双指针应用, 冒泡排序'''
+    # nums = [-444, -56, -8, 90, 299, 576, 2348, 7890]
+    # ans_double = algo.sortedSquares_double(algo, nums)
+    # ans_bubble = algo.sortedSquares_bubble(algo, nums)
+    # print(f'ans_double is : {ans_double}, time complexity: O(n)')
+    # print(f'ans_bubble is : {ans_bubble}, time complexity: O(n**2)')
+
